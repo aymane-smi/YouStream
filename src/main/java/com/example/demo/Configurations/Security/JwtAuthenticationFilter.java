0fb8aedd1f01dc03103541f8e3680776733d5a63
belassiria aymane <aymanebel2@outlook.fr>
2024-03-28 10:49:34 +0000

    [YS-43] add exception handling for servletdisaptcher anf for filter

--- src/main/java/com/example/demo/Configurations/Security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/Configurations/Security/JwtAuthenticationFilter.java
@@ -1,19 +1,20 @@
 package com.example.demo.Configurations.Security;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.example.demo.Models.Entites.Student;
+import com.example.demo.Exceptions.UnAuthorizedException;
 import com.example.demo.Services.impl.AdminDetailsImpl;
 import com.example.demo.Services.impl.StudentDetailsImpl;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -40,35 +41,51 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException{
-        String header = request.getHeader("Authorization");
-        String userType = request.getParameter("userType");
-        final String token;
-        if (header == null || !header.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        token = header.replace("Bearer ", "");
-        jwtService.setToken(token);
-        String username = jwtService.extractUsername(token);
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetailsVar = null;
-            if(userType.equals("student"))
-                userDetailsVar = studentDetailsImpl.loadUserByUsername(username);
-            else if(userType.equals("admin"))
-                userDetailsVar = adminDetailsImpl.loadUserByUsername(username);
-            if(userDetailsVar == null)
-                throw new ServletException("userDetails is null");
-            if (jwtService.validateToken(token, userDetailsVar)) {
-                UsernamePasswordAuthenticationToken authenticationToken = null;
-                authenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetailsVar, null, userDetailsVar.getAuthorities()
-                    );
-                authenticationToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        try{
+            String header = request.getHeader("Authorization");
+            String userType = request.getParameter("userType");
+            final String token;
+            if (header == null || !header.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
             }
+            token = header.replace("Bearer ", "");
+            jwtService.setToken(token);
+            String username = jwtService.extractUsername(token);
+            if(username.isEmpty())
+                throw new UnAuthorizedException("unauthorized");
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetailsVar = null;
+                if(userType.equals("student"))
+                    userDetailsVar = studentDetailsImpl.loadUserByUsername(username);
+                else if(userType.equals("admin"))
+                    userDetailsVar = adminDetailsImpl.loadUserByUsername(username);
+                if(userDetailsVar == null)
+                    throw new UnAuthorizedException("userDetails is null");
+                if (jwtService.validateToken(token, userDetailsVar)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = null;
+                    authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetailsVar, null, userDetailsVar.getAuthorities()
+                        );
+                    authenticationToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }else{
+                    throw new UnAuthorizedException("unathorized");
+                }
+            }
+            filterChain.doFilter(request, response);
+        }catch(UnAuthorizedException ex){
+            response.setStatus(403);
+            response.setContentType("application/json");
+            response.getWriter().write("{'error': 'unauthorized'}");
+
+        }catch(ExpiredJwtException ex){
+            response.setStatus(403);
+            response.setContentType("application/json");
+            response.getWriter().write("{'error': 'expired'}");
+
         }
-        filterChain.doFilter(request, response);
     }
 }
